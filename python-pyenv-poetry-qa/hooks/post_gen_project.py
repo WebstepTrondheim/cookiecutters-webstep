f5ca1a6e55c2e3fd037d3fdd01cafac06013ced1
Tim Jagenberg <tim.jagenberg@webstep.no>
2020-07-10 10:46:58 +0200

    git init and symlinking pre-commit hook

--- python-pyenv-poetry-qa/hooks/post_gen_project.py
+++ python-pyenv-poetry-qa/hooks/post_gen_project.py
@@ -4,26 +4,42 @@ import subprocess
 import sys
 
 try:
-    st = os.stat('qa.sh')
-    os.chmod('qa.sh', st.st_mode | stat.S_IEXEC)
+    subprocess.check_call(["git", "init"])
+except subprocess.CalledProcessError as err:
+    print('ERROR: git exited with %s!' % err)
+
+
+try:
+    st = os.stat("qa.sh")
+    os.chmod("qa.sh", st.st_mode | stat.S_IEXEC)
 except OSError as err:
-    print('ERROR: Could not make file executable. %s!' % err)
+    print("ERROR: Could not make file executable. %s!" % err)
+    sys.exit(1)
+
+
+try:
+    os.symlink("../../qa.sh", ".git/hooks/pre-commit")
+except OSError as err:
+    print("ERROR: Could not symlink git pre-commit hook. %s!" % err)
     sys.exit(1)
 
 
 try:
     subprocess.check_call(["pyenv", "install", "--skip-existing", "3.8.3"])
 except subprocess.CalledProcessError as err:
-    print('ERROR: pyenv exited with %s!' % err)
+    print("ERROR: pyenv exited with %s!" % err)
+    sys.exit(1)
 
 
 try:
     subprocess.check_call(["pyenv", "local", "3.8.3"])
 except subprocess.CalledProcessError as err:
-    print('ERROR: pyenv exited with %s!' % err)
+    print("ERROR: pyenv exited with %s!" % err)
+    sys.exit(1)
 
 
 try:
     subprocess.check_call(["poetry", "update"])
 except subprocess.CalledProcessError as err:
-    print('ERROR: poetry exited with %s!' % err)
+    print("ERROR: poetry exited with %s!" % err)
+    sys.exit(1)
